a655e3ecc91952cc7bcfa01207f93cf0d8238233
TSRaghu <traghuvaran@languageline.com>
2018-12-07 10:15:53 -0800

    12-07-2018
    First github commit from local

---
+++ src/Divisors.py
@@ -0,0 +1,22 @@
+# Exercise 4 - Get a number as input from user and 
+# 12-07-2018
+
+#Get user input
+Userdigit = int(input("Please enter your number: "))
+
+#Create a list with range of numbers less than user digit to identify divisors
+listrange = list(range(1, Userdigit+1))
+
+#Create a second list to store the results
+divisorlist=[]
+
+#Identify all the divisors
+for i in listrange:
+    if Userdigit % i == 0:
+        divisorlist.append(i)
+       
+#Print new list
+print (divisorlist)
+
+   
+
---
+++ src/Listlessthanten.py
@@ -0,0 +1,22 @@
+# Exercise 3 - Get a number from list and add any number less than user entered input in another list
+# 12-06-2018
+
+#Create a list with ten numbers
+numberlist = [1 , 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+#Get user input
+Userdigit = int(input("Please enter your number: "))
+
+#Create Second list to store numbers less than the user entered digit
+newlist=[]
+
+#Compare User entered digit with numbers in the list
+for i in numberlist:
+    if Userdigit < i:
+            newlist.append(i)
+
+#Print new list
+print (newlist)
+
+   
+
